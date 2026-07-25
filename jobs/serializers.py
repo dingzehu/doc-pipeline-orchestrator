4436ba2e73524566a403923e4a70e7ca4585cd1a
Ding-Ze Hu <hudingtza@gmail.com>
2026-07-25 11:40:24 +0200

    serializers.py

---
+++ jobs/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Job
+
+
+class JobSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Job
+        fields = ["id", "filename", "status", "result_json", "error", "created_at"]
+        read_only_fields = ["id", "status", "result_json", "error", "created_at"]
