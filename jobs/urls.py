b70574f6a3abc7c9cb6798285b43a1575905a6b9
Ding-Ze Hu <hudingtza@gmail.com>
2026-07-30 16:08:18 +0200

    setups

---
+++ jobs/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("upload/", views.UploadView.as_view(), name="upload"),
+    path("status/<int:pk>/", views.StatusView.as_view(), name="status"),
+    path("jobs/", views.JobListView.as_view(), name="job-list"),
+    path("ask/", views.AskView.as_view(), name="ask"),
+]
